3ffd2486fd01afa3ebc93818da4fd4a1eeac353b
Kong Fanyue <fykong@thoughtworks.com>
2019-07-09 11:41:51 +0800

    [fykong]-Refactor \ Rename Change Test

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,15 +1,15 @@
-const receiptSummary = require('../main');
+const printReceipt = require('../main');
 
 it ('should return error msg when receipt id is invalid', () => {
-    expect(receiptSummary([])).toBe('ERROR Invalid codes');
+    expect(printReceipt([])).toBe('ERROR Invalid codes');
 });
 
 it ('should return error msg when receipt id is invalid', () => {
-    expect(receiptSummary(['ERROR'])).toBe('ERROR Invalid codes');
+    expect(printReceipt(['ERROR'])).toBe('ERROR Invalid codes');
 });
 
 it ('should return receipt summary info when id is valid', () => {
-    expect(receiptSummary(['0001'])).toBe('Receipts\n'+
+    expect(printReceipt(['0001'])).toBe('Receipts\n'+
     '------------------------------------------------------------\n'+
     'Coca Cola\t\t3\t\t1\n'+
     '------------------------------------------------------------\n'+
@@ -17,7 +17,7 @@ it ('should return receipt summary info when id is valid', () => {
 });
 
 it ('should return multi-lines receipt summary info when ids are valid', () => {
-    expect(receiptSummary(['0001', '0003', '0005', '0003'])).toBe('Receipts\n'+
+    expect(printReceipt(['0001', '0003', '0005', '0003'])).toBe('Receipts\n'+
     '------------------------------------------------------------\n'+
     'Coca Cola\t\t3\t\t1\n'+
     'Pepsi-Cola\t\t5\t\t2\n'+
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function receiptSummary(productIdArray) {
+function printReceipt(productIdArray) {
     if (!(productIdArray instanceof Array) || productIdArray.length == 0) {
         return "ERROR Invalid codes"
     }
@@ -70,18 +70,7 @@ function groupProductArray(productArray) {
 }
 
 function findProductInfo(ids) {
-    var list = [
-        { "id": "0001", "name": "Coca Cola", "price": 3 },
-        { "id": "0002", "name": "Diet Coke", "price": 4 },
-        { "id": "0003", "name": "Pepsi-Cola", "price": 5 },
-        { "id": "0004", "name": "Mountain Dew", "price": 6 },
-        { "id": "0005", "name": "Dr Pepper", "price": 7 },
-        { "id": "0006", "name": "Sprite", "price": 8 },
-        { "id": "0007", "name": "Diet Pepsi", "price": 9 },
-        { "id": "0008", "name": "Diet Mountain Dew", "price": 10 },
-        { "id": "0009", "name": "Diet Dr Pepper", "price": 11 },
-        { "id": "0010", "name": "Fanta", "price": 12 }
-    ];
+    var list = loadAllItems();
 
     var result = [];
     ids.forEach(id => {
@@ -94,6 +83,21 @@ function findProductInfo(ids) {
     return result;
 }
 
+function loadAllItems() {
+    return [
+        { "id": "0001", "name": "Coca Cola", "price": 3 },
+        { "id": "0002", "name": "Diet Coke", "price": 4 },
+        { "id": "0003", "name": "Pepsi-Cola", "price": 5 },
+        { "id": "0004", "name": "Mountain Dew", "price": 6 },
+        { "id": "0005", "name": "Dr Pepper", "price": 7 },
+        { "id": "0006", "name": "Sprite", "price": 8 },
+        { "id": "0007", "name": "Diet Pepsi", "price": 9 },
+        { "id": "0008", "name": "Diet Mountain Dew", "price": 10 },
+        { "id": "0009", "name": "Diet Dr Pepper", "price": 11 },
+        { "id": "0010", "name": "Fanta", "price": 12 }
+    ];
+}
+
 function findProduct(list, id) {
     for (let index = 0; index < list.length; index++) {
         if (list[index].id == id) {
@@ -104,4 +108,4 @@ function findProduct(list, id) {
 }
 
 
-module.exports = receiptSummary;
+module.exports = printReceipt;
